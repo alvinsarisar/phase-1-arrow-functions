cd602564dcfd0cbf7d8533c5673b797d3e2707d6
alvin <alvin.sarisar@student.moringaschool.com>
2024-06-30 00:45:38 +0300

    pointer

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+
+// Function expression called divide
+  let a = 2000
+  let b = 100
+const divide = function(a, b) {
+    return a / b;
+  }
+  
+  // Arrow function called square
+  let x = 2
+  const square = x => x * x;
+  
+  // Arrow function called add
+  let c = 3
+  let d = 4 
+  const add = (c, d) => c + d ;
+  
+
+  
